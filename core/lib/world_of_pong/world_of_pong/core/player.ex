0714e70fc5a237467d24688266f5f1df0b3b39ca
mr-rampage <readonly@che.wbac.ca>
2019-06-16 00:56:50 -0400

    (feat) guarding against invalid readings

--- core/lib/world_of_pong/world_of_pong/core/player.ex
+++ core/lib/world_of_pong/world_of_pong/core/player.ex
@@ -6,7 +6,8 @@ defmodule WorldOfPong.Core.Player do
   """
 
   @type t :: %Player{ name: String.t, readings: list( pos_integer ) }
-  defstruct name: "Player", readings: [ ]
+  defstruct name: "Player",
+            readings: [ ]
 
   @doc """
   Creates a player with a given name
@@ -26,14 +27,19 @@ defmodule WorldOfPong.Core.Player do
   ## Example
 
     iex> add_reading(%Player{name: "Fred"}, 5)
-    %Player{name: "Fred", readings: [5]}
+    { :ok, %Player{name: "Fred", readings: [5]} }
+    iex> add_reading(%Player{name: "Fred"}, -1)
+    { :error }
 
   """
-  @spec add_reading( %Player{ }, pos_integer ) :: %Player{ }
-  def add_reading( %Player{ readings: old_readings } = player, reading ) do
+  @spec add_reading( %Player{ }, pos_integer ) :: { :ok, %Player{ } }
+  def add_reading( %Player{ readings: old_readings } = player, reading ) when reading >= 0 do
     new_readings = [ reading | old_readings ]
-    %Player{ player | readings: new_readings }
+    { :ok, %Player{ player | readings: new_readings } }
   end
+
+  @spec add_reading( %Player{ }, neg_integer ) :: { :error }
+  def add_reading( _player, _reading ), do: { :error }
 
   @doc """
   Clears all readings from a player
@@ -42,33 +48,28 @@ defmodule WorldOfPong.Core.Player do
 
     iex> charlie = %Player{name: "Charlie", readings: [15]};
     iex> clear_readings(charlie);
-    %Player{name: "Charlie", readings: []}
+    { :ok, %Player{name: "Charlie", readings: []} }
 
   """
   @spec clear_readings( %Player{ } ) :: %Player{ }
-  def clear_readings( player ) do
-    %Player{ player | readings: [ ] }
-  end
+  def clear_readings( %Player{ } = player ), do: { :ok, %Player{ player | readings: [ ] } }
 
   @doc """
   Calculates the average position of a player
 
   ## Example
 
-    iex> charlie = %Player{name: "Charlie", readings: [15, 10, 11, 13, 15]};
+    iex> charlie = %Player{name: "Charlie", readings: [15, 10, 11, 13, 15]}
     iex> average_reading(charlie);
     {:ok, 12.8}
     iex> average_reading(%Player{name: "Charlie", readings: []})
     {:error}
 
   """
-  @spec average_reading( %Player{ } ) :: { :error }
-  def average_reading( %Player{ readings: [ ] } ) do
-    { :error }
-  end
-
   @spec average_reading( %Player{ } ) :: { :ok, float }
-  def average_reading( %Player{ readings: readings } ) do
-    { :ok, Enum.sum( readings ) / length( readings ) }
-  end
+  def average_reading( %Player{ readings: [ _ | _ ] = readings } ), do: { :ok, Enum.sum( readings ) / length( readings ) }
+
+  @spec average_reading( %Player{ } ) :: { :error }
+  def average_reading( _player ), do: { :error }
+
 end
