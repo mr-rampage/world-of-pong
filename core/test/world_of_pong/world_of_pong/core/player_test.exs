01050b3df31fba0d57ad3006e00d17e71590bb16
mr-rampage <readonly@che.wbac.ca>
2019-06-16 22:24:25 -0400

    (test) clean up

--- core/test/world_of_pong/world_of_pong/core/player_test.exs
+++ core/test/world_of_pong/world_of_pong/core/player_test.exs
@@ -5,35 +5,25 @@ defmodule WorldOfPong.Core.PlayerTest do
   alias WorldOfPong.Core.Player
   doctest WorldOfPong.Core.Player, import: true
 
-  property "given a non-empty name, always create a Player" do
-    forall x <- :proper_types.string() do
-      Player.new( x ) == %Player{ name: x, readings: [ ] }
-    end
-  end
+  @tim %Player{ name: "Tim", readings: [ 5 ] }
 
-  property "given a positive reading, always prepend to the player's readings" do
-    tim = %Player{ name: "Tim", readings: [ 5 ] }
-    forall x <- :proper_types.pos_integer() do
-      Player.add_reading( tim, x ) == { :ok, %Player{ name: "Tim", readings: [ x, 5 ] } }
-    end
-  end
+  property "given a non-empty name, always create a Player", do:
+    forall x <- :proper_types.string( ), do:
+      equals( %Player{ name: x, readings: [ ] }, Player.new( x ) )
 
-  property "given a negative reading, always error out" do
-    tim = %Player{ name: "Tim", readings: [ 5 ] }
-    forall x <- :proper_types.neg_integer() do
-      Player.add_reading( tim, x ) == { :error }
-    end
-  end
+  property "given a positive reading, always prepend to the player's readings", do:
+    forall x <- pos_integer( ), do:
+      equals( { :ok, %Player{ @tim | readings: [ x, 5 ] } }, Player.add_reading( @tim, x ) )
 
-  property "given a Player with readings, always clear the readings" do
-    forall readings <- list(pos_integer()) do
-      Player.clear_readings(%Player{ name: "Tim", readings: readings }) == {:ok, Player.new("Tim") }
-    end
-  end
+  property "given a negative reading, always error out", do:
+    forall x <- neg_integer( ), do:
+      equals( { :error }, Player.add_reading( @tim, x ) )
 
-  property "given a Player with readings, always calculate the average reading" do
-    forall readings <- non_empty(list(pos_integer())) do
-      elem(Player.average_reading(%Player{ name: "Tim", readings: readings }), 0) == :ok
-    end
-  end
+  property "given a Player with readings, always clear the readings", do:
+    forall readings <- list( pos_integer( ) ), do:
+      equals( { :ok, Player.new( "Tim" ) }, Player.clear_readings( %Player{ @tim | readings: readings } ) )
+
+  property "given a Player with readings, always calculate the average reading", do:
+    forall readings <- non_empty( list( pos_integer( ) ) ), do:
+      equals( :ok, elem( Player.average_reading( %Player{ @tim | readings: readings } ), 0 ) )
 end
