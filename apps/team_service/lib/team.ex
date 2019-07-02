0e392ac97b0388491cdd2fa5879da4fcf9ab6a84
mr-rampage <readonly@che.wbac.ca>
2019-07-01 23:28:34 -0400

    (feat) adding members to team

--- apps/team_service/lib/team.ex
+++ apps/team_service/lib/team.ex
@@ -5,7 +5,7 @@ defmodule TeamService.Team do
   alias __MODULE__
 
   @type side :: :left | :right
-  @type team :: %Team{team: side, members: list(pid())}
+  @type team :: %Team{team: side, members: list(pid)}
   defstruct team: nil, members: []
 
   use GenServer
@@ -13,13 +13,13 @@ defmodule TeamService.Team do
   @spec start_link(side) :: GenServer.on_start()
   def start_link(team), do: GenServer.start_link(__MODULE__, team)
 
-  @spec add_player(pid) :: :ok
-  def add_player(pid), do: GenServer.cast(pid, {:add_player, pid})
+  @spec add_member(pid) :: :ok
+  def add_member(member), do: GenServer.cast(__MODULE__, {:add_member, member})
 
   @impl true
   def init(team), do: {:ok, %Team{team: team}}
 
   @impl true
-  def handle_cast({:add_player, pid}, %Team{members: members} = team),
-    do: {:noreply, %Team{team | members: [pid | members]}}
+  def handle_cast({:add_member, member}, %Team{members: members} = team), do:
+      {:noreply, %Team{team | members: [member | members]}}
 end
