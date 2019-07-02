2ca7fad2cb349c0cfcb632754198113e401a53ec
mr-rampage <readonly@che.wbac.ca>
2019-07-01 22:47:38 -0400

    (fix) type conflict

--- apps/team_service/lib/team.ex
+++ apps/team_service/lib/team.ex
@@ -4,16 +4,14 @@ defmodule TeamService.Team do
   """
   alias __MODULE__
 
-  @type team :: :left|:right
-
-  @type team :: %Team{team: team, members: list(pid())}
+  @type side :: :left | :right
+  @type team :: %Team{team: side, members: list(pid())}
   defstruct team: nil, members: []
 
   use GenServer
 
-  @spec start_link(team) :: GenServer.on_start
+  @spec start_link(side) :: GenServer.on_start()
   def start_link(team), do: GenServer.start_link(__MODULE__, team)
-
 
   @spec add_player(pid) :: :ok
   def add_player(pid), do: GenServer.cast(pid, {:add_player, pid})
@@ -22,5 +20,6 @@ defmodule TeamService.Team do
   def init(team), do: {:ok, %Team{team: team}}
 
   @impl true
-  def handle_cast({:add_player, pid}, %Team{members: members} = team), do: {:noreply, %Team{team | members: [pid | members]}}
+  def handle_cast({:add_player, pid}, %Team{members: members} = team),
+    do: {:noreply, %Team{team | members: [pid | members]}}
 end
